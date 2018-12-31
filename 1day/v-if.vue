c11150c1111ca909a5296c7fa5f37dfcc7ce6b8a
hainanzhang <1379114708@qq.com>
2018-12-31 13:16:02 +0800

    第一天

---
+++ 1day/v-if.vue
@@ -0,0 +1,19 @@
+<script src="https://cdn.staticfile.org/vue/2.5.21/vue.common.dev.js"></script>
+
+<template>
+	<h1>title</h1>
+	<p>paragraph 1</p>
+	<p>paragraph 2</p>
+</template>
+
+<script>
+	var exampleVM2=new Vue({
+		el:'#app',
+		data:{
+			greeting:true
+		}
+	})
+</script>
+
+<style>
+</style>
